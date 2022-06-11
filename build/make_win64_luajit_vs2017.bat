de27b9a81e09c429f14f2993b14a742fb994828a
Smilexs <smilexs.zzs@foxmail.com>
2022-06-11 14:23:28 +0800

    fix make_win64_luajit_vs2017

--- build/make_win64_luajit_vs2017.bat
+++ build/make_win64_luajit_vs2017.bat
@@ -1,6 +1,4 @@
-@echo off
-
-call "D:\VisualStudio2017\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64
 
 echo Swtich to x64 build env
 cd %~dp0\luajit-2.1.0b3\src
@@ -9,7 +7,6 @@ cd ..\..
 
 mkdir build_lj64 & pushd build_lj64
 cmake -DUSING_LUAJIT=ON -G "Visual Studio 15 2017 Win64" ..
-IF %ERRORLEVEL% NEQ 0 cmake -DUSING_LUAJIT=ON -G "Visual Studio 15 2017 Win64" ..
 popd
 cmake --build build_lj64 --config Release
 md plugin_luajit\Plugins\x86_64
